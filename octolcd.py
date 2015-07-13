a133febb82c17b8a4d46b006ddb89ce82af79a43
Matthew McMillan <matthew.mcmillan@gmail.com>
2015-07-13 10:59:01 -0500

    Switched over to @mikestreety pypi-lcdscreen library

--- octolcd.py
+++ octolcd.py
@@ -9,13 +9,6 @@
 # http://matthewcmcmillan.blogspot.com
 #--------------------------------------
 #
-#--------------------------------------
-# 20x4 LCD code
-#
-# Author : Matt Hawkins
-#
-# http://www.raspberrypi-spy.co.uk/
-#--------------------------------------
 
 # The wiring for the LCD is as follows:
 # 1 : GND
@@ -35,51 +28,23 @@
 # 15: LCD Backlight +5V**
 # 16: LCD Backlight GND
 
-#import
-import RPi.GPIO as GPIO
 import time
 import requests
 from subprocess import *
+from lcdscreen import LCDScreen
 
 # Define GPIO to LCD mapping
-LCD_RS = 25
-LCD_E  = 24
-LCD_D4 = 23
-LCD_D5 = 17
-LCD_D6 = 21
-LCD_D7 = 22
-LED_ON = 15
+lcd = LCDScreen({
+        'pin_rs': 25,
+        'pin_e': 24,
+        'pins_db': [23, 17, 27, 22],
+        'backlight': 18,
+        'dimensions': [20, 4]
+})
 
-# Define some device constants
-LCD_WIDTH = 20    # Maximum characters per line
-LCD_CHR = True
-LCD_CMD = False
-
-LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
-LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
-LCD_LINE_3 = 0x94 # LCD RAM address for the 3rd line
-LCD_LINE_4 = 0xD4 # LCD RAM address for the 4th line
-
-# Timing constants
-E_PULSE = 0.0005
-E_DELAY = 0.0005
 
 def main():
   # Main program block
-
-  # Configure GPIO Pins as outputs
-  GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
-  GPIO.setup(LCD_E, GPIO.OUT)  # EN
-  GPIO.setup(LCD_RS, GPIO.OUT) # RS
-  GPIO.setup(LCD_D4, GPIO.OUT) # DB4
-  GPIO.setup(LCD_D5, GPIO.OUT) # DB5
-  GPIO.setup(LCD_D6, GPIO.OUT) # DB6
-  GPIO.setup(LCD_D7, GPIO.OUT) # DB7
-  GPIO.setup(LED_ON, GPIO.OUT) # Backlight enable
-
-  # Initialise display
-  lcd_init()
-
 
   while True:
     #Get IP address of the OctoPrint server
@@ -132,102 +97,16 @@ def main():
                 printpercentmsg = str(printpercent) + '%'
 
     # Write data to the LCD screen
-    lcd_string(ipaddrmsg,LCD_LINE_1,2)
-    lcd_string(hotmsg,LCD_LINE_2,1)
-    lcd_string(bedmsg,LCD_LINE_3,1)
+    lcd.message(ipaddrmsg, 'left')
+    lcd.message(hotmsg, 'left')
+    lcd.message(bedmsg, 'center')
     if printeronline:
-        lcd_string(printtimemsg + '        ' + printpercentmsg,LCD_LINE_4,1)
+        lcd.message(printtimemsg + '        ' + printpercentmsg, 'left')
     else:
-        lcd_string('',LCD_LINE_4,1)
+        lcd.message('', 'left')
 
     time.sleep(3) # 3 second delay
 
-
-    # Blank display
-    #lcd_byte(0x01, LCD_CMD)
-
-
-def lcd_init():
-  # Initialise display
-  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
-  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
-  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
-  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
-  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
-  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
-  time.sleep(E_DELAY)
-
-def lcd_byte(bits, mode):
-  # Send byte to data pins
-  # bits = data
-  # mode = True  for character
-  #        False for command
-
-  GPIO.output(LCD_RS, mode) # RS
-
-  # High bits
-  GPIO.output(LCD_D4, False)
-  GPIO.output(LCD_D5, False)
-  GPIO.output(LCD_D6, False)
-  GPIO.output(LCD_D7, False)
-  if bits&0x10==0x10:
-    GPIO.output(LCD_D4, True)
-  if bits&0x20==0x20:
-    GPIO.output(LCD_D5, True)
-  if bits&0x40==0x40:
-    GPIO.output(LCD_D6, True)
-  if bits&0x80==0x80:
-    GPIO.output(LCD_D7, True)
-
-  # Toggle 'Enable' pin
-  lcd_toggle_enable()
-
-  # Low bits
-  GPIO.output(LCD_D4, False)
-  GPIO.output(LCD_D5, False)
-  GPIO.output(LCD_D6, False)
-  GPIO.output(LCD_D7, False)
-  if bits&0x01==0x01:
-    GPIO.output(LCD_D4, True)
-  if bits&0x02==0x02:
-    GPIO.output(LCD_D5, True)
-  if bits&0x04==0x04:
-    GPIO.output(LCD_D6, True)
-  if bits&0x08==0x08:
-    GPIO.output(LCD_D7, True)
-
-  # Toggle 'Enable' pin
-  lcd_toggle_enable()
-
-def lcd_toggle_enable():
-  # Toggle enable
-  time.sleep(E_DELAY)
-  GPIO.output(LCD_E, True)
-  time.sleep(E_PULSE)
-  GPIO.output(LCD_E, False)
-  time.sleep(E_DELAY)
-
-def lcd_string(message,line,style):
-  # Send string to display
-  # style=1 Left justified
-  # style=2 Centred
-  # style=3 Right justified
-
-  if style==1:
-    message = message.ljust(LCD_WIDTH," ")
-  elif style==2:
-    message = message.center(LCD_WIDTH," ")
-  elif style==3:
-    message = message.rjust(LCD_WIDTH," ")
-
-  lcd_byte(line, LCD_CMD)
-
-  for i in range(LCD_WIDTH):
-    lcd_byte(ord(message[i]),LCD_CHR)
-
-def lcd_backlight(flag):
-  # Toggle backlight on-off-on
-  GPIO.output(LED_ON, flag)
 
 def getipaddr():
         cmd = "ip addr show eth0 | grep inet | awk '{print $2}' | cut -d/ -f1"
@@ -237,7 +116,7 @@ def getipaddr():
 
 def cleanup():
     lcd_byte(0x01, LCD_CMD)
-    lcd_string("Shutdown",LCD_LINE_1,2)
+    lcd.message("Shutdown", 'center')
     GPIO.cleanup()
 
 if __name__ == '__main__':
